19058906b9ebaae1bb973c35aec9adfdc096eda8
cfrpg <726525185@qq.com>
2019-06-10 17:24:33 +0800

    Add TabPage.

---
+++ src/SharpBladeFlightAnalyzer/Controls/TabPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SharpBladeFlightAnalyzer
+{
+	/// <summary>
+	/// TabPage.xaml 的交互逻辑
+	/// </summary>
+	public partial class TabPage : TabItem
+	{
+		private TabControl parent;
+
+		private double defaultWidth = 100;
+
+		public TabPage()
+		{
+			InitializeComponent();
+		}
+
+		private void TabItem_Loaded(object sender, RoutedEventArgs e)
+		{
+			parent = findParent(this);
+			if (parent != null)
+				Init();
+		}
+
+		private void Init()
+		{
+			parent.SizeChanged += Parent_SizeChanged;
+			parent.SelectionChanged += Parent_SelectionChanged;
+			setWidth();
+		}
+
+		private void Parent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			setWidth();
+			e.Handled = true;
+		}
+
+		private void Parent_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			setWidth();
+		}
+
+		private void setWidth()
+		{
+			double w = (parent.ActualWidth - 20) / parent.Items.Count;			
+			if (w > defaultWidth)
+				w = defaultWidth;
+			else		
+				w = (parent.ActualWidth - 20 - (defaultWidth - w + 5)) / parent.Items.Count;				
+			
+			foreach(TabPage item in parent.Items)
+			{
+				if (item.IsSelected)
+					item.Width = defaultWidth;
+				else
+					item.Width = w;
+			}
+		}
+
+		private TabControl findParent(DependencyObject curr)
+		{
+			DependencyObject dobj = VisualTreeHelper.GetParent(curr);
+			if (dobj == null)
+				return null;
+			if (dobj.GetType() == typeof(TabControl))
+				return dobj as TabControl;
+			return findParent(dobj);
+		}
+
+		private void btn_Close_Click(object sender, RoutedEventArgs e)
+		{
+			if (parent == null)
+				return;
+            if (!IsSelected)
+                return;
+			parent.Items.Remove(this);
+			parent.SizeChanged -= Parent_SizeChanged;
+			parent.SelectionChanged -= Parent_SelectionChanged;
+			setWidth();
+		}
+	}
+}
--- src/SharpBladeFlightAnalyzer/MainWindow.xaml.cs
+++ src/SharpBladeFlightAnalyzer/MainWindow.xaml.cs
@@ -12,17 +12,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace SharpBladeFlightAnalyzer
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();			
+		}
+	}
 }
