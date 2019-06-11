82ec8d6d1b4834a2a8ce398835b0696061db0111
cfrpg <726525185@qq.com>
2019-06-11 11:41:27 +0800

    大致折腾了一下界面
    RT

--- src/SharpBladeFlightAnalyzer/Controls/TabPage.xaml.cs
+++ src/SharpBladeFlightAnalyzer/Controls/TabPage.xaml.cs
@@ -15,82 +15,122 @@ using System.Windows.Shapes;
 
 namespace SharpBladeFlightAnalyzer
 {
-	/// <summary>
-	/// TabPage.xaml 的交互逻辑
-	/// </summary>
-	public partial class TabPage : TabItem
-	{
-		private TabControl parent;
+    /// <summary>
+    /// TabPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class TabPage : TabItem
+    {
+        private TabControl parent;
+        private double defaultWidth = 150;
 
-		private double defaultWidth = 100;
+        public static readonly DependencyProperty CloseEnabledProperty =
+            DependencyProperty.Register("CloseEnabled", typeof(bool), typeof(TabPage), new PropertyMetadata(true, TabPage.OnCloseEnabledPropertyChanged));
+        public bool CloseEnabled
+        {
+            get { return (bool)GetValue(CloseEnabledProperty); }
+            set { SetValue(CloseEnabledProperty, value); }
+        }
+        public static void OnCloseEnabledPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            TabPage s = sender as TabPage;
+            s.OnCloseEnabledChanged(e);
+        }
+        public void OnCloseEnabledChanged(DependencyPropertyChangedEventArgs e)
+        {
+            setCloseBtn();
+        }
 
-		public TabPage()
-		{
-			InitializeComponent();
-		}
+        public TabPage()
+        {
+            InitializeComponent();
+        }
 
-		private void TabItem_Loaded(object sender, RoutedEventArgs e)
-		{
-			parent = findParent(this);
-			if (parent != null)
-				Init();
-		}
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            setCloseBtn();
+        }
 
-		private void Init()
-		{
-			parent.SizeChanged += Parent_SizeChanged;
-			parent.SelectionChanged += Parent_SelectionChanged;
-			setWidth();
-		}
+        private void setCloseBtn()
+        {
+            Button b = (Button)GetTemplateChild("btn_Close");
+            if (b == null)
+                return;
+            if (CloseEnabled)
+            {
+                b.Visibility = Visibility.Visible;
+                b.IsEnabled = true;
+            }
+            else
+            {
+                b.Visibility = Visibility.Hidden;
+                b.IsEnabled = false;
+            }
+        }
 
-		private void Parent_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			setWidth();
-			e.Handled = true;
-		}
+        private void TabItem_Loaded(object sender, RoutedEventArgs e)
+        {
+            parent = findParent(this);
+            if (parent != null)
+                Init();
+        }
 
-		private void Parent_SizeChanged(object sender, SizeChangedEventArgs e)
-		{
-			setWidth();
-		}
+        private void Init()
+        {
+            parent.SizeChanged += Parent_SizeChanged;
+            parent.SelectionChanged += Parent_SelectionChanged;
+            setWidth();
+        }
 
-		private void setWidth()
-		{
-			double w = (parent.ActualWidth - 20) / parent.Items.Count;			
-			if (w > defaultWidth)
-				w = defaultWidth;
-			else		
-				w = (parent.ActualWidth - 20 - (defaultWidth - w + 5)) / parent.Items.Count;				
-			
-			foreach(TabPage item in parent.Items)
-			{
-				if (item.IsSelected)
-					item.Width = defaultWidth;
-				else
-					item.Width = w;
-			}
-		}
+        private void Parent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            setWidth();
+            e.Handled = true;
+        }
 
-		private TabControl findParent(DependencyObject curr)
-		{
-			DependencyObject dobj = VisualTreeHelper.GetParent(curr);
-			if (dobj == null)
-				return null;
-			if (dobj.GetType() == typeof(TabControl))
-				return dobj as TabControl;
-			return findParent(dobj);
-		}
+        private void Parent_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            setWidth();
+        }
 
-		private void btn_Close_Click(object sender, RoutedEventArgs e)
-		{
-			if (parent == null)
-				return;
+        private void setWidth()
+        {
+            double w = (parent.ActualWidth - 20) / parent.Items.Count;
+
+            if (w > defaultWidth)
+                w = defaultWidth;
+            else
+                w = (parent.ActualWidth - 20 - (defaultWidth - w + 5)) / parent.Items.Count;
+
+            foreach (TabPage item in parent.Items)
+            {
+                if (item.IsSelected)
+                    item.Width = defaultWidth;
+                else
+                    item.Width = w;
+            }
+        }
+
+        private TabControl findParent(DependencyObject curr)
+        {
+            DependencyObject dobj = VisualTreeHelper.GetParent(curr);
+            if (dobj == null)
+                return null;
+            if (dobj.GetType() == typeof(TabControl))
+                return dobj as TabControl;
+            return findParent(dobj);
+        }
+
+        private void btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            if (parent == null)
+                return;
             if (!IsSelected)
                 return;
-			parent.Items.Remove(this);
-			parent.SizeChanged -= Parent_SizeChanged;
-			parent.SelectionChanged -= Parent_SelectionChanged;
-			setWidth();
-		}
-	}
+            parent.Items.Remove(this);
+            parent.SizeChanged -= Parent_SizeChanged;
+            parent.SelectionChanged -= Parent_SelectionChanged;
+            setWidth();
+        }
+    }
 }
